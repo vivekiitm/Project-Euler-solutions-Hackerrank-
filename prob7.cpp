8b81fd41b12579510f60096dd239a01e96742747
Vivek Moar <44374277+vivekiitm@users.noreply.github.com>
2020-01-30 17:55:16 +0530

    Add files via upload

---
+++ prob7.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_SIZE 1000005
+
+void SieveOfEratosthenes(vector<int> &primes)
+{
+	bool IsPrime[MAX_SIZE];
+	memset(IsPrime, true, sizeof(IsPrime));
+
+	for (int p = 2; p * p < MAX_SIZE; p++)
+	{
+		if (IsPrime[p] == true)
+		{
+			for (int i = p * p; i < MAX_SIZE; i += p)
+				IsPrime[i] = false;
+		}
+	}
+	for (int p = 2; p < MAX_SIZE; p++)
+	if (IsPrime[p])
+			primes.push_back(p);
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+	vector<int> primes;
+	SieveOfEratosthenes(primes);
+    for(int i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        cout<< primes[n-1] << endl;
+    }
+	return 0;
+}
